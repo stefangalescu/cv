03fdc403a6c6ae8fac422fdc6d3e7ba2a5eb9c75
stefangalescu <stefan.galescu@gmail.com>
2024-01-26 20:56:34 +0200

    feat: add image modifiers support to `AvatarImage`

--- components/ui/avatar/AvatarImage.vue
+++ components/ui/avatar/AvatarImage.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
+import type { ImageModifiers } from '@nuxt/image'
 import { AvatarImage, type AvatarImageProps } from 'radix-vue'
 
-const props = defineProps<Omit<AvatarImageProps, 'as'>>()
+const props = defineProps<Omit<AvatarImageProps, 'as'> & Partial<ImageModifiers>>()
 
 const img = useImage()
 // eslint-disable-next-line unicorn/prevent-abbreviations
-const src = computed(() => img(props.src))
+const src = computed(() => img(props.src, { fit: props.fit, width: props.width, height: props.height, format: props.format }))
+const imgAttributes = computed(() => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { format, ...rest } = props
+  return { ...rest, src: src.value }
+})
 </script>
 
 <template>
-  <AvatarImage v-bind="{ ...props, src }" class="h-full w-full object-cover" />
+  <AvatarImage v-bind="imgAttributes" class="h-full w-full object-cover" />
 </template>

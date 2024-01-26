116aeb6a9704f735fdbf342b6528cf91e6ed9ef9
stefangalescu <stefan.galescu@gmail.com>
2024-01-26 19:19:34 +0200

    chore: update nuxt config
    - enable modules
    - add google fonts config
    - add site config
    - add `lib` to auto imports
    - add image module config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,53 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  image: {
+    domains: ['avatars.githubusercontent.com'],
+    format: ['avif', 'webp'],
+  },
+
+  imports: {
+    dirs: ['lib'],
+    mergeExisting: true,
+  },
+
+  modules: ['@nuxt/image', '@nuxtjs/eslint-module', '@nuxtjs/google-fonts', '@nuxtjs/seo', '@nuxtjs/tailwindcss', 'radix-vue/nuxt', 'nuxt-icon', 'shadcn-nuxt'],
+
+  googleFonts: {
+    families: {
+      Inter: {
+        wght: '100..900',
+      },
+    },
+    download: true,
+    inject: true,
+    preload: true,
+    stylePath: 'assets/fonts.css',
+    fontsDir: 'assets/fonts',
+    fontsPath: 'fonts',
+  },
+
+  site: {
+    name: 'Stefan Galescu | Full Stack Product Engineer',
+    url: 'https://cv.stefangalescu.com',
+  },
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: 'Ui',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui',
+  },
+
+  $development: {
+    site: {
+      url: 'http://localhost:3000',
+    },
+  },
 })
